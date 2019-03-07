08490898e4e9e83ab20a98910783b9350210c95f
Miha <mihadebeljak4@gmail.com>
2019-03-07 10:58:34 +0100

    Initial Commit

---
+++ Udder1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder1
+//
+//  Created by Miha Debeljak on 07/03/2019.
+//  Copyright © 2019 mihad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
